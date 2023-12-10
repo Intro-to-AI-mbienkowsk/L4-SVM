a07de6cbbacfbce5cde9a842179f0e935c4d8778
maksym <01178511@pw.edu.pl>
2023-12-10 21:36:21 +0100

    Refactor SVM class and implement MnistSVM subclass
    The SVM class is refactored to be more versatile with a more flexible initialization process. Changes include renaming 'lbda' to 'lambda_param' and allowing the dimension to be variable rather than fixed. The number recognition functionality has been moved into a new subclass, MnistSVM, to better segregate responsibilities. This refactor addresses code readability and maintainability while preserving functionality.

--- SVM.py
+++ SVM.py
@@ -4,39 +4,38 @@ import abc
 
 
 class SVM:
-    def __init__(self, num_to_recognize: int, lbda=DEFAULT_LAMBDA, learning_rate=DEFAULT_LEARNING_RATE,
+    def __init__(self, lambda_param=DEFAULT_LAMBDA, learning_rate=DEFAULT_LEARNING_RATE, dimension=784,
                  batch_size=2 ** DEFAULT_BATCH_EXPONENT, epochs=DEFAULT_EPOCHS, training_method=TRAINING_METHOD.BGD):
-        self.num_to_recognize = num_to_recognize
-        self.weights = np.zeros(784)
-        self.lbda = lbda
+        self.weights = np.zeros(dimension)
+        self.lambda_param = lambda_param
         self.bias = 0
         self.learning_rate = learning_rate
         self.batch_size = batch_size
         self.epochs = epochs
         self.training_method = training_method
+        self.dimension = dimension
         self.losses = []
 
     def fit(self, x, y):
         # if a number is not the one we're looking for, it is classified as -1
-        bin_y = self.get_binary_y(y)
-        w = np.zeros(784)
+        w = np.zeros(self.dimension)
         b = 0
         for i in range(self.epochs):
             if self.training_method == TRAINING_METHOD.SGD:
                 for idx, x_i in enumerate(x):
-                    is_correct = bin_y[idx] * (np.dot(x_i, self.weights) + self.bias) >= 1
+                    is_correct = y[idx] * (np.dot(x_i, self.weights) + self.bias) >= 1
                     if is_correct:
                         # shrink weight vector to maximize separation
-                        self.weights -= self.learning_rate * (2 * self.lbda * self.weights)
+                        self.weights -= self.learning_rate * (2 * self.lambda_param * self.weights)
                     else:
-                        self.weights -= self.learning_rate * (2 * self.lbda * self.weights - np.dot(x_i, bin_y[idx]))
-                        self.bias -= self.learning_rate * bin_y[idx]
+                        self.weights -= self.learning_rate * (2 * self.lambda_param * self.weights - np.dot(x_i, y[idx]))
+                        self.bias -= self.learning_rate * y[idx]
 
             elif self.training_method == TRAINING_METHOD.BGD:
 
                 for batch_start in range(0, x.shape[0], self.batch_size):
-                    grad_w_r_t_w = 0
-                    grad_w_r_t_b = 0
+                    grad_w = 0
+                    grad_b = 0
 
                     for j in range(batch_start, batch_start + self.batch_size):
                         if j >= x.shape[0]:
@@ -44,10 +43,10 @@ class SVM:
                         ti = y[j] * (np.dot(self.weights, x[j].T) + self.bias)
                         if ti < 1:
                             # incorrect classification
-                            grad_w_r_t_w += self.lbda * y[j] * x[j]
-                            grad_w_r_t_b += self.lbda * y[j]
-                    w = w - self.learning_rate * w + self.learning_rate * grad_w_r_t_w
-                    b = b + self.learning_rate * grad_w_r_t_b
+                            grad_w += self.lambda_param * y[j] * x[j]
+                            grad_b += self.lambda_param * y[j]
+                    w = w - self.learning_rate * w + self.learning_rate * grad_w
+                    b = b + self.learning_rate * grad_b
                 self.weights = w
                 self.bias = b
 
@@ -55,15 +54,23 @@ class SVM:
             print(f"Epoch {i}, loss {self.losses[-1]}")
 
     def hinge_loss(self, x, y):
-        bin_y = self.get_binary_y(y)
-        distances = 1 - bin_y * (np.dot(x, self.weights) - self.bias)
+        distances = 1 - y * (np.dot(x, self.weights) - self.bias)
         losses = np.maximum(0, distances)
-        regularization = self.lbda * REGULARIZATION_SCALAR * np.dot(self.weights, self.weights)
+        regularization = self.lambda_param * REGULARIZATION_SCALAR * np.dot(self.weights, self.weights)
         mean_loss = np.mean(losses) + regularization
         return mean_loss
 
     def predict(self, x):
         return int(np.sign(np.dot(x, self.weights) + self.bias))
+
+    def score(self, x, y):
+        return sum([int(self.predict(x[i]) == int(y[i])) for i in range(x.shape[0])]) / x.shape[0]
+
+
+class MnistSVM(SVM):
+    def __init__(self, *args, **kwargs):
+        self.num_to_recognize = kwargs.pop('num_to_recognize')
+        super().__init__(*args, dimension=784, **kwargs)
 
     def get_binary_y(self, y):
         """Maps the given label array onto a binary one containing 1 if the number is equal
@@ -72,5 +79,10 @@ class SVM:
         return np.where(y != self.num_to_recognize, -1, 1)
 
     def score(self, x, y):
-        y_ = self.get_binary_y(y)
-        return sum([int(self.predict(x[i]) == int(y_[i])) for i in range(x.shape[0])]) / x.shape[0]
+        return super().score(x, self.get_binary_y(y))
+
+    def hinge_loss(self, x, y):
+        return super().hinge_loss(x, self.get_binary_y(y))
+
+    def fit(self, x, y):
+        return super().fit(x, self.get_binary_y(y))
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from SVM import SVM
+from SVM import SVM, MnistSVM
 from util import import_data
 
 
 def main():
-    svm = SVM(0, epochs=100, learning_rate=0.001)
+    svm = MnistSVM(num_to_recognize=1, epochs=25, learning_rate=0.0001)
     (train_x, train_y), (test_x, test_y) = import_data()
     svm.fit(train_x, train_y)
     score = svm.score(test_x, test_y)
