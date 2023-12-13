9b96d4beff8ffb94748f94c83e31ab3db01f1230
maksym <01178511@pw.edu.pl>
2023-12-13 13:03:23 +0100

    Add constants and implement kernel functions in DualSVM
    The code now includes some default constants defined in util.py and incorporates their use in DualSVM.py. The Gaussian and Polynomial kernels functions in DualSVM have also been implemented using these constants and dependencies.

--- DualSVM.py
+++ DualSVM.py
@@ -1,5 +1,6 @@
 import numpy as np
 from util import *
+from scipy.spatial.distance import cdist
 from sklearn import datasets
 
 
@@ -11,10 +12,14 @@ class DualSVM:
                  learning_rate=DEFAULT_LEARNING_RATE,
                  epochs=DEFAULT_EPOCHS,
                  num_to_recognize=DEFAULT_NUM_TO_RECOGNIZE):
-        ...
+        self.sigma = sigma
+        self.c = c
+        self.learning_rate = learning_rate
+        self.epochs = epochs
+        self.num = num_to_recognize
 
     def _gaussian_kernel(self, x1, x2):
-        ...
+        return np.exp(-cdist(x1, x2) / 2 * self.sigma ** 2)
 
     def _polynomial_kernel(self, x1, x2, degree):
         return (x1 @ x2 + POLYNOMIAL_KERNEL_CONSTANT_TERM) ** degree
