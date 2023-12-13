606330812b90fa840411b41818996026faaae507
maksym <01178511@pw.edu.pl>
2023-12-13 12:52:21 +0100

    polynomial kernel

--- DualSVM.py
+++ DualSVM.py
@@ -9,14 +9,15 @@ class DualSVM:
                  sigma=DEFAULT_SIGMA,
                  degree=DEFAULT_DEGREE,
                  learning_rate=DEFAULT_LEARNING_RATE,
-                 epochs=DEFAULT_EPOCHS):
+                 epochs=DEFAULT_EPOCHS,
+                 num_to_recognize=DEFAULT_NUM_TO_RECOGNIZE):
         ...
 
     def _gaussian_kernel(self, x1, x2):
         ...
 
     def _polynomial_kernel(self, x1, x2, degree):
-        ...
+        return (x1 @ x2 + POLYNOMIAL_KERNEL_CONSTANT_TERM) ** degree
 
     def fit(self, x, y):
         ...
