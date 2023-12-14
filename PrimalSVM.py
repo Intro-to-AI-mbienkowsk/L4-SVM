b1b7ab778c5e32eb68003413c3fa671851dd23c8
maksym <01178511@pw.edu.pl>
2023-12-14 12:10:17 +0100

    refactoring

--- PrimalSVM.py
+++ PrimalSVM.py
@@ -1,16 +1,17 @@
+import matplotlib.pyplot as plt
 import numpy as np
 from util import *
 
 
 class PrimalSVM:
     def __init__(self, training_data, training_labels, c=DEFAULT_C, learning_rate=DEFAULT_LEARNING_RATE, dimension=784,
-                 epochs=DEFAULT_EPOCHS, training_method=TRAINING_METHOD.SGD):
+                 epochs=DEFAULT_EPOCHS):
+        # self.weights = np.random.random(dimension)
         self.weights = np.zeros(dimension)
         self.c = c
         self.bias = 0
         self.learning_rate = learning_rate
         self.epochs = epochs
-        self.training_method = training_method
         self.dimension = dimension
         self.losses = []
         self.train_x = training_data
@@ -18,23 +19,35 @@ class PrimalSVM:
 
     def fit(self):
         x, y = self.train_x, self.train_y
+        best_weights = self.weights
+        best_bias = self.bias
+        best_loss = float('inf')
+
         for i in range(self.epochs):
-            print(f'epoch {i}')
-            if self.training_method == TRAINING_METHOD.SGD:
-                margin = y * self.decision_function(x)
-                missclassified_indices = np.where(margin < 1)[0]
+            margin = y * self.decision_function(x)
+            missclassified_indices = np.where(margin < 1)[0]
 
-                grad_w = self.weights - self.c * y[missclassified_indices].dot(x[missclassified_indices])
-                self.weights = self.weights - grad_w * self.learning_rate
+            dw = self.weights - self.c * (x[missclassified_indices].T * y[missclassified_indices]).T.sum(axis=0)
+            db = - self.c * np.sum(y[missclassified_indices])
 
-                grad_b = self.c * np.sum(y[missclassified_indices])
-                self.bias = self.bias - grad_b * self.learning_rate
+            self.weights -= self.learning_rate * dw
+            self.bias -= self.learning_rate * db
 
-            self.losses.append(self.hinge_loss())
+            loss = self.hinge_loss()
+            self.losses.append(loss)
+            if loss < best_loss:
+                best_loss = loss
+                best_bias = self.bias
+                best_weights = self.weights
+
+            print(
+                f"epoch {i}, loss {self.losses[-1]}, dw={round(np.linalg.norm(dw), 2)}, db={round(np.linalg.norm(db), 2)}")
+        self.weights = best_weights
+        self.bias = best_bias
 
     def hinge_loss(self):
-        return (np.dot(self.weights, self.weights) + self.c * np.sum(
-            np.maximum(0, 1 - self.train_y * self.decision_function(self.train_x)))) / 2
+        return (np.dot(self.weights / 2, self.weights) + self.c * np.sum(
+            np.maximum(0, 1 - self.train_y * self.decision_function(self.train_x))))
 
     def predict(self, x):
         return np.sign(self.decision_function(x))
@@ -49,11 +62,22 @@ class PrimalSVM:
 class MnistPrimalSVM(PrimalSVM):
     def __init__(self, *args, **kwargs):
         self.target_num = kwargs.pop('num_to_recognize')
-        train_y = kwargs.pop('training_data')
-        super().__init__(*args, dimension=784, training_data=self.binary_labels(train_y), **kwargs)
+        train_y = kwargs.pop('training_labels')
+        super().__init__(*args, dimension=784, training_labels=self.binary_labels(train_y), **kwargs)
 
     def binary_labels(self, y):
         """Maps the given label array onto a binary one containing 1 if the number is equal
         to the number we're trying to classify, else -1
         """
         return np.where(y != self.target_num, -1, 1)
+
+    def plot_losses(self):
+        fig = plt.figure()
+        fig.set_facecolor("lightblue")
+        xlabels = np.arange(len(self.losses))
+        plt.plot(xlabels, self.losses, color='#2596be')
+        plt.title(f"Hinge loss in each epoch, searching for {self.target_num}", weight="bold")
+        plt.xlabel("epoch nr")
+        plt.ylabel("loss")
+        plt.grid(True)
+        plt.show()
