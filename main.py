f9128bb759feb7cec20083a1a8beb46e816d2b21
maksym <01178511@pw.edu.pl>
2023-12-14 12:09:43 +0100

    update main

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-from SVM import SVM, MnistSVM
-from util import import_data
+from MnistClassifier import MnistClassifier
 
 
 def main():
-    svm = MnistSVM(num_to_recognize=1, epochs=25, learning_rate=0.0001)
-    (train_x, train_y), (test_x, test_y) = import_data()
-    svm.fit(train_x, train_y)
-    score = svm.score(test_x, test_y)
-    print(f"Accuracy: {round(score, 2)}")
+    classifier = MnistClassifier(primal=True)
+    classifier.train()
+    print(f"Accuracy: {round(classifier.score(), 2)}")
+    classifier.display_confusion_matrix()
 
 
 if __name__ == '__main__':
